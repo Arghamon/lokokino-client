d9a81c17be20778b27677dfc017fb20623984954
Ioane Lomidze <ioanelomidze@gmail.com>
2020-09-17 13:27:26 +0400

    change to heroku api

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const configureAxios = () => {
   const instance = axios.create();
 
-  // instance.defaults.baseURL = process.env.REACT_APP_BASE_URL;
-  instance.defaults.baseURL = 'http://localhost:4000';
+  instance.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+  // instance.defaults.baseURL = 'http://localhost:4000';
 
   instance.interceptors.request.use((request) => {
 
